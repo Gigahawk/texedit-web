74ad0d681b19e40a0cbe114c1e7ff2e4914584f1
Miro <MiroPhoenix91@gmail.com>
2021-01-21 16:53:31 +0100

    modified

--- app.js
+++ app.js
@@ -1,25 +1,52 @@
 const container = document.querySelector('.container')
+window.addEventListener('onload', creatDivs(16,16));
+const boxs = container.querySelectorAll('div')
 
 function creatDivs(col , rows) {
-
     for(let i = 0;i < (col * rows); i++) {
-        const div = document.createElement('div')
+        const div = document.createElement('div') 
         container.style.gridTemplateColumns = `repeat(${col}, 1fr)`;
         container.style.gridTemplateRows = `repeat(${rows}, 1fr)`;
         div.style.border = '1px solid red'
         container.appendChild(div).classList.add('box')
     }
-
-    const boxs = container.querySelectorAll('div')
-    boxs.forEach(box => box.addEventListener('mouseover', changeColor))
+    // boxs.forEach(box => box.addEventListener('mouseover', changeColor))
 }
 
-creatDivs(16,16)
 
-function changeColor() {
 
-    let RNum = Math.floor(Math.random() * 256);
-    let RGB = `rgb(${RNum},${RNum},${RNum})`
-    this.style.background = RGB;
-    console.log(RGB)
+
+
+
+
+
+function grayColor() { 
+    const btn = document.createElement('button')
+    btn.textContent = "Gray"
+    btn.addEventListener('click', () => {
+        boxs.forEach(box => box.addEventListener('mouseover', ()=> {
+            let RNum = Math.floor(Math.random() * 256);
+            let GrayScale = `rgb(${RNum},${RNum},${RNum})`
+            box.style.background = GrayScale;
+        }))
+    })
+   document.body.appendChild(btn).classList.add('btn')
 }
+grayColor()
+
+
+function rgbColors() {
+    const btn = document.createElement('button')
+    btn.textContent = "RGB"
+    btn.addEventListener('click' ,() => {
+        boxs.forEach(box => box.addEventListener('mouseover', () => {
+            let R = Math.floor(Math.random() * 256);
+            let G = Math.floor(Math.random() * 256);
+            let B = Math.floor(Math.random() * 256);
+            const RGB = `rgb(${R},${G},${B})`;
+            box.style.background = RGB;
+        }))
+    })
+    document.body.appendChild(btn).classList.add('btn')
+}
+rgbColors()

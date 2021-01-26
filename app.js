7a632adfdff5d4ef2fce0d6305f253f7c9f786d5
Miro <MiroPhoenix91@gmail.com>
2021-01-26 20:34:15 +0100

    uf

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const btnBlack = document.createElement('button')
 const btnRgb = document.createElement('button')
 const btnGray = document.createElement('button')
 const btnSize = document.createElement('button')
+const section = document.querySelector('.section');
+const buttonsContainer = document.querySelector('.buttons');
 
 
 function creatDivs(col , rows) {
@@ -10,7 +12,6 @@ function creatDivs(col , rows) {
         const div = document.createElement('div') 
         container.style.gridTemplateColumns = `repeat(${col}, 1fr)`;
         container.style.gridTemplateRows = `repeat(${rows}, 1fr)`;
-        div.style.border = '1px solid red'
         container.appendChild(div).classList.add('box')
     }
 }
@@ -27,7 +28,7 @@ function grayColor() {
             box.style.background = GrayScale;
         }))
     })
-   document.body.appendChild(btnGray).classList.add('btn')
+    buttonsContainer.appendChild(btnGray).classList.add('btn')
 }
 grayColor()
 
@@ -45,7 +46,7 @@ function rgbColors() {
             box.style.background = RGB;
         }))
     })
-    document.body.appendChild(btnRgb).classList.add('btn')
+    buttonsContainer.appendChild(btnRgb).classList.add('btn')
 }
 rgbColors()
 
@@ -58,7 +59,7 @@ function blackColor() {
             this.style.background = 'black'
         }))
     })
-    document.body.appendChild(btnBlack).classList.add('btn')
+    buttonsContainer.appendChild(btnBlack).classList.add('btn')
 }
 blackColor() 
 
@@ -89,6 +90,30 @@ function reSize() {
             blackColor();
         }
     })
-    document.body.appendChild(btnSize).classList.add('btn')
+    buttonsContainer.appendChild(btnSize).classList.add('btn')
 }
 reSize()
+
+function backgrooundAnim() {
+
+    colors = ['red','green','blue', 'yellow'];
+    const section = document.querySelector('.section');
+    const span = document.createElement('span');
+
+    let size = Math.random() * 50;
+
+    span.style.width = 10 + size + 'px';
+    span.style.height = 10 + size + 'px';
+    span.style.borderRadius = `${size}%`;
+    span.style.top = Math.random() * innerHeight + "px";
+    span.style.left = Math.random() * innerWidth + "px";
+
+    const bg = colors[Math.floor(Math.random() * colors.length)];
+    span.style.background = bg;
+
+    section.appendChild(span);
+
+    setTimeout(() => {span.remove()},5000)
+}
+
+setInterval(backgrooundAnim, 150);
